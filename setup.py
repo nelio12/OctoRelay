3c6ca88595d13558b11deef9a03a93d668a8f88e
nelio12 <57465206+nelio12@users.noreply.github.com>
2023-02-24 17:09:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ plugin_url = "https://github.com/borisbu/OctoRelay"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["RPi.GPIO"]
+plugin_requires = ["OPi.GPIO"]
 
 # --------------------------------------------------------------------------------------------------------------------
 # More advanced options that you usually shouldn't have to touch follow after this point
